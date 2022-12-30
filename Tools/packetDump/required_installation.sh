2b10d4bc813899c921395291026cc6aeba790624
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2022-12-30 16:37:25 +0900

    Update required_installation.sh
    modi Explanation

--- Tools/packetDump/required_installation.sh
+++ Tools/packetDump/required_installation.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-apt update && apt-get update && apt-get install libpcap-dev -y && apt-get install make -y && apt-get install g++ -y && ./k8s_Kubernetes/Tools/packetDump/required_installation.sh && make && ls
+apt update && apt-get update && apt-get install libpcap-dev -y && apt-get install make -y && apt-get install g++ -y && make
+echo "[+] packetDump installation is complete!! @IMyoungho"
+echo "[+] Usage: ./packetDump [interface name] "
+ls
