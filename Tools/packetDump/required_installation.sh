8aea7dfa1679e61b11e70bf9648963bfd92cc28e
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2022-12-30 16:31:35 +0900

    Update required_installation.sh

--- Tools/packetDump/required_installation.sh
+++ Tools/packetDump/required_installation.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-apt update && apt-get update && apt-get install libpcap-dev -y && apt-get install make -y && apt-get install g++ -y
-chmod +x k8s_Kubernetes/Tools/packetDump/required_installation.sh
-./k8s_Kubernetes/Tools/packetDump/required_installation.sh
-make
-ls
+apt update && apt-get update && apt-get install libpcap-dev -y && apt-get install make -y && apt-get install g++ -y && ./k8s_Kubernetes/Tools/packetDump/required_installation.sh && make && ls
