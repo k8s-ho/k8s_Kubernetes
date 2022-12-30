29fe2f38c89721b6b5715731d7974f1f24bc68b1
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2022-12-30 16:12:18 +0900

    Create required_installation.sh
    required installation

---
+++ Tools/packetDump/required_installation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt update && apt-get update && apt-get install libpcap-dev -y 
+&& apt-get install make -y && apt-get install g++ -y 
+&& git clone https://github.com/k8s-ho/k8s_Kubernetes
