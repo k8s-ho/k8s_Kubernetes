a7d994c727f8c44a74464a4120499bdcc0cfbc63
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2022-12-30 16:40:01 +0900

    Update required_installation.sh

--- Tools/packetDump/required_installation.sh
+++ Tools/packetDump/required_installation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt update && apt-get update && apt-get install libpcap-dev -y && apt-get install make -y && apt-get install g++ -y && make
-echo "[+] packetDump installation is complete!! @IMyoungho"
-echo "[+] Usage: ./packetDump [interface name] "
+clear
+echo "[*] packetDump installation is complete!! @IMyoungho"
+echo "[?] Usage: ./packetDump [interface name] "
 ls
