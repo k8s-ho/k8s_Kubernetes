571b36ab8455c3cf0d55926e11bc78f3df4b2977
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-10 00:56:57 +0900

    Update imageChecker.sh
    modi, func

--- Tools/imageChecker/imageChecker.sh
+++ Tools/imageChecker/imageChecker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 POD_COUNT=$(kubectl get pod -A -o jsonpath='{.items[*].metadata.name}' | sed 's/ /\n/g' | wc -w) # Pod의 갯수 변수저장 -> Pod 명을 같을 수 없음
 N_IMAGE_COUNT=$(kubectl get pod -A -o jsonpath='{.items[*].spec.containers[*].image}' | sed 's/ /\n/g' | sort | uniq | wc -w) # Image의 갯수 변수 저장 -> 중복이 있을 수 있으니 제거
 D_IMAGE_COUNT=$(kubectl get pod -A -o jsonpath='{.items[*].status.containerStatuses[*].imageID}' | sed 's/ /\n/g' | sort | uniq | wc -w) # Image의 갯수 변수 저장 -> 중복이 있을 수 있으니 제거
@@ -32,42 +32,54 @@ echo " => Enter the Namespace: "
 read NS
 echo "[+] The namespace you chose is $NS !!"
 
-CHECK=$(kubectl get pod -n $NS -o jsonpath='{.items[*].status.containerStatuses[*].image}' | wc -w)
-if [ $CHECK -le 0 ]; then
-	echo "[!] ERROR: namespace with no or no pods in the namespace."
-	exit 100
-fi
+image_func()
+{
+	mkdir download_image
+	cd download_image
 
-CHECK2=$(systemctl status docker | wc -l)
-if [ $CHECK2 -eq 0 ]; then
+	if [ $1 == "namespace" ]; then
+		kubectl get pod -n $NS -o jsonpath='{.items[*].spec.containers[*].image}' | sed 's/ /\n/g' | sort | uniq > image.txt
+	elif [ $1 == "all" ]; then
+		kubectl get pod -A -o jsonpath='{.items[*].spec.containers[*].image}' | sed 's/ /\n/g' | sort | uniq > image.txt
+	fi
+	while read LINE; do
+		echo "[+] Start image pull: [$LINE]"
+		docker pull -q $LINE
+	done < image.txt
+	echo " "
+	echo " "
+	while read LINE; do
+		echo "[+] Save the image in tar format: [$LINE]"
+		docker save $LINE > $(echo $LINE | sed 's/\//_/g' | sed 's/\./_/g' | sed 's/\:/-/g' ).tar # tar error by name
+	done < image.txt
+	echo " "
+	echo " "
+
+	while read LINE; do
+		echo "[+] Storing image vulnerability information summary using trivy: [$LINE]"
+		trivy image -q $LINE | grep Total: -B3 >> vuln_image.txt
+		echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"  >> vuln_image.txt
+	done < image.txt
+	echo " "
+	echo "[*] Storing imageat ~/download_image/vuln_image.txt"
+	rm -rf image.txt
+}
+
+CHECK=$(systemctl status docker | wc -l)
+if [ $CHECK -eq 0 ]; then
 	echo "[!] ERROR: You need docker installation!! Run the following command"
 	echo "[+] apt update && apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin"
 	echo "[+] systemctl start docker"
 	exit 100
 fi
 
-mkdir download_image
-cd download_image
-
-kubectl get pod -n $NS -o jsonpath='{.items[*].spec.containers[*].image}' | sed 's/ /\n/g' | sort | uniq > image.txt
-while read LINE; do
-	echo "[+] Start image pull: [$LINE]"
-	docker pull -q $LINE
-done < image.txt
-echo " "
-echo " "
-while read LINE; do
-	echo "[+] Save the image in tar format: [$LINE]"
-	docker save $LINE > $(echo $LINE | sed 's/\//_/g').tar
-done < image.txt
-echo " "
-echo " "
-
-while read LINE; do
-	echo "[+] Storing image vulnerability information summary using trivy: [$LINE]"
-	trivy image -q $LINE | grep Total: -B3 >> vuln_image.txt
-	echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"  >> vuln_image.txt
-done < image.txt
-echo " "
-echo "[*] Storing imageat ~/download_image/vuln_image.txt"
-rm -rf image.txt
+if [ $NS == "all" ] || [ $NS == "ALL" ] || [ $NS == "All" ] || [ $NS == "aLL" ]; then
+	image_func "all"
+else
+	CHECK2=$(kubectl get pod -n $NS -o jsonpath='{.items[*].status.containerStatuses[*].image}' | wc -w)
+	if [ $CHECK2 -le 0 ]; then
+		echo "[!] ERROR: namespace with no or no pods in the namespace."
+		exit 100
+	fi
+	image_func "namespace"
+fi
