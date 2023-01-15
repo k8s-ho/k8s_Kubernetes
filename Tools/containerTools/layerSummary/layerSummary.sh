6e21c4f07f176837c49d95d80ca0608bd7e37212
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-15 17:31:23 +0900

    Update layerSummary.sh
    modi, seq

--- Tools/containerTools/layerSummary/layerSummary.sh
+++ Tools/containerTools/layerSummary/layerSummary.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # option description
 usage()
 {
@@ -8,31 +9,45 @@ usage()
 	exit 100
 }
 
+parse(){
+	while read LINE; do
+		cat /var/lib/docker/image/overlay2/layerdb/sha256/$LINE/$1 >> ./result_file/$1.txt; echo " " >> ./result_file/$1.txt
+	done < ./result_file/layerdb.txt
+	echo "[+] layerdb.$1" 
+	echo "[>] Path: /var/lib/docker/image/overlay2/layerdb/sha256/*/$1"
+	echo "------------------------------------------------------"
+	cat ./result_file/$1.txt | sort
+	echo " "
+	echo " "
+}
+
 # show all data
 showAll(){
-	parse(){
-		while read LINE; do
-			cat /var/lib/docker/image/overlay2/layerdb/sha256/$LINE/$1 >> ./result_file/$1.txt; echo " " >> ./result_file/$1.txt
-		done < ./result_file/layerdb.txt
-	
-		echo "[+] layerdb.$1 " 
-		echo "[>] Path: /var/lib/docker/image/overlay2/layerdb/sha256/*/$1"
-		echo "------------------------------------------------------"
-		cat ./result_file/$1.txt | sort
-		echo " "
-		echo " "
-	}
-	
-	echo "[+] Real Data" 
+	CHK=$(ls | grep -i result_file | wc -l)
+	if [ $CHK -eq 1 ]; then
+		rm -rf result_file
+	fi 
+	mkdir result_file
+	ls /var/lib/docker/image/overlay2/layerdb/sha256/ > ./result_file/layerdb.txt
+	echo " "
+	echo "[+] Real Data / The actual data pointed to by layerdb.cache-id" 
+	echo "              / LowerDir + UpperDir + Merge Dir values are referenced here"
+	echo " "
 	echo "[>] Path: /var/lib/docker/overlay2/*"
 	echo "------------------------------------------------------"
 	ls /var/lib/docker/overlay2 | sort
 	echo " "
 	echo " "
-	
 	parse cache-id
 	parse diff
 }
+
+# chekcing docker image & result_file exist
+PROC_CHK=$(docker images | wc -l)
+if [ $PROC_CHK -le 1 ]; then
+	echo "[!] Error: Check if the docker image exists"
+	exit 100
+fi 
 
 # Run option 
 while getopts ah opts; do
@@ -44,20 +59,7 @@ while getopts ah opts; do
         esac
 done
 
-# chekcing docker image & result_file exist
-PROC_CHK=$(docker images | wc -l)
-if [ $PROC_CHK -le 1 ]; then
-	echo "[!] Error: Check if the docker image exists"
-	exit 100
-fi 
-CHK=$(ls | grep -i result_file | wc -l)
-if [ $CHK -eq 1 ]; then
-	rm -rf result_file
-fi 
-
 # Extract layer from docker inspect
-mkdir result_file
-ls /var/lib/docker/image/overlay2/layerdb/sha256/ > ./result_file/layerdb.txt
 echo " "
 echo "[+] docker layers infomation - mapping images"
 echo "------------------------------------------------------"
@@ -75,9 +77,9 @@ for row in $(echo "${data}" | jq -r '.[] | @base64'); do
  
   echo "* Image: $IMG" | tr -d "[]\""
 	echo "* Image ID: $ID" | tr -d "\n"
-	echo "* RootFS Layer $ROOTFS" | tr -d "[]\"\," | sort
+	echo "* RootFS Layer (Same as layerdb.diff) $ROOTFS" | tr -d "[]\"\," | sort
 	echo " "
-	echo "* LowerDir "
+	echo "* LowerDir"
 	echo "$LOWER" | sed 's/\:/\n/g' | sort | tr -d '"' 
 	echo " "
 	echo "* UpperDir "
