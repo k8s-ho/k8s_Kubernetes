58a49237d8748522d8fc66809c6ec47a1cce2411
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-01-15 15:19:10 +0900

    Create setup_layerSummary.sh
    add, setup layerSummary

---
+++ Tools/containerTools/setup_layerSummary.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+apt update && apt-get install docker-ce docker-ce-cli docker-compose-plugin -y
+apt install jq
+systemctl restart docker
+chmod +x layerSummary.sh
+clear
+echo "[*] layerSummary setup installation is complete!! @IMyoungho"
+echo "[?] Usage: ./layerSummary.sh"
+ls
