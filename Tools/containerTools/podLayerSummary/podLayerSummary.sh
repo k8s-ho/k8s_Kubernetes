23fe7df47db664b108463cf1465813cca67686b0
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-02-06 15:51:19 +0900

    Update podLayerSummary.sh

--- Tools/containerTools/podLayerSummary/podLayerSummary.sh
+++ Tools/containerTools/podLayerSummary/podLayerSummary.sh
@@ -3,9 +3,10 @@ VAR=$(crictl ps -q)
 array=($VAR)
 for var in "${array[@]}"
 do
-  crictl inspect $(crictl ps --id $var -q) | jq -r '"[POD NAME] " + .status.metadata.name, "[POD PID] " + (.info.pid | tostring)'
-  echo "[+] Container ID= $var"
-  echo "[+] Container data base path = /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/snapshots/"
+  crictl inspect $(crictl ps --id $var -q) | jq -r '"[POD NAME] " + .info.runtimeSpec.annotations."io.kubernetes.cri.sandbox-name", "[Container NAME] " + .info.runtimeSpec.annotations."io.kubernetes.cri.container-name", "[Container PID] " + (.info.pid | tostring)'
+  echo "[Container ID] $var"
+  echo "[Container Data Path] /var/lib/containerd/io.containerd.snapshotter.v1.overlayfs/snapshots"
+  echo "[+] Container Data list"
   cat /proc/$(crictl inspect $(crictl ps --id $var -q) | jq '.info.pid')/mounts | grep -i overlay | sed 's/:/\n/g' | sed 's/,/\n/g' | sed 's/\/var\/lib\/containerd\/io.containerd.snapshotter.v1.overlayfs\/snapshots\///g'
   echo " "
   echo "==============================================================="
