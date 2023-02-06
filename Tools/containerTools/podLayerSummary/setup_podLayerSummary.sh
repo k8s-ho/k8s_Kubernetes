74a606686f3dceae850ca31608a2e7f5f25bf4bc
@IMyoungho <118821939+k8s-ho@users.noreply.github.com>
2023-02-06 16:30:15 +0900

    Update setup_podLayerSummary.sh

--- Tools/containerTools/podLayerSummary/setup_podLayerSummary.sh
+++ Tools/containerTools/podLayerSummary/setup_podLayerSummary.sh
@@ -19,6 +19,7 @@ echo "[*] podLayerSummary setup installation is complete!! @IMyoungho"
 echo "[?] Usage: ./podLayerSummary.sh"
 echo "<options>"
 echo "    -a     : Show all information"
+echo "    -f     : Filter and Show. ./podLayerSummary.sh -f [Container ID]"
 echo "    -h     : Show this message."
 echo "------------------------------------------------------------"
 ls
